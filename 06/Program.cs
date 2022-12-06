241c65a39560282f784ba9b1ea79c08de84e2a6f
Stefan Kraus <stefan.kraus@methodpark.de>
2022-12-06 12:06:29 +0100

    Add day 06

---
+++ 06/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+
+    const int MARKER_LEN = 14;
+
+    static void Main(String[] args)
+    {
+        String line = Console.ReadLine()!;
+        for (int i = 0; i + MARKER_LEN < line.Length; i++)
+        {
+            HashSet<char> x = new HashSet<char>();
+            for (int j = 0; j < MARKER_LEN; j++)
+            {
+                x.Add(line[i + j]);
+            }
+            if (x.Count() == MARKER_LEN)
+            {
+                Console.WriteLine(i + MARKER_LEN);
+                return;
+            }
+        }
+    }
+}
