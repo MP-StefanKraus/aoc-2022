e5dd6849b57f8823b5f71d68891c76e7aabeabb0
Stefan Kraus <stefan.kraus@methodpark.de>
2022-12-10 13:28:17 +0100

    Add day 10

---
+++ 10/Program01.cs
@@ -0,0 +1,43 @@
+﻿class Program01
+{
+
+    private static int count = 0;
+    private static int X = 1;
+
+    private static int result = 0;
+
+    private static int[] relevant = new int[] { 20, 60, 100, 140, 180, 220 };
+
+    private static void increaseCountAndCheck()
+    {
+        count++;
+        if (relevant.Contains(count))
+        {
+            result += count * X;
+
+        }
+
+    }
+
+    static void PrevMain(String[] args)
+    {
+        String line;
+
+        while ((line = Console.ReadLine()) != null)
+        {
+            if (line == "noop")
+            {
+                increaseCountAndCheck();
+            }
+            else
+            {
+                increaseCountAndCheck();
+                increaseCountAndCheck();
+                int num = Convert.ToInt32(line.Split()[1]);
+                X += num;
+            }
+
+        }
+        Console.WriteLine(result);
+    }
+}
---
+++ 10/Program02.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+
+    static int count = 0;
+    static int X = 1;
+
+    static List<char> res = new List<char>();
+
+    static void drawAndIncrease()
+    {
+        res.Add(Math.Abs(count % 40 - X) <= 1 ? '#' : '.');
+        count++;
+    }
+
+    static void Main(String[] args)
+    {
+        String line;
+
+        while ((line = Console.ReadLine()) != null)
+        {
+            if (line == "noop")
+            {
+                drawAndIncrease();
+            }
+            else
+            {
+                drawAndIncrease();
+                drawAndIncrease();
+                int num = Convert.ToInt32(line.Split()[1]);
+                X += num;
+            }
+
+        }
+        for (int i = 0; i < res.Count(); i++)
+        {
+            if (i % 40 == 0)
+            {
+                Console.Write('\n');
+            }
+            Console.Write(res[i]);
+        }
+    }
+}
